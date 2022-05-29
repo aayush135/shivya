01f2e2e9cfcb716c4457282a98021d1460d21ff9
asnk <singh.asnk@gmail.com>
2022-05-30 01:56:01 +0530

    updated docker files

--- server/api/src/routes/index.js
+++ server/api/src/routes/index.js
@@ -1,5 +1,5 @@
 module.exports=async(app)=>{
     app.use('/account',require('./account'))
-    app.use('/',(req,res)=>{res.send('api docs')})
+    app.use('/',(req,res)=>{res.send('api docs!!')})
     app.use('/*',(req,res)=>{res.sendStatus(404)})
 }
