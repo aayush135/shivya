05a6684e7f21136b132fb6d6edcbae487153bc82
asnk <singh.asnk@gmail.com>
2022-05-30 03:14:53 +0530

    updated docker compose file

--- server/api/config/index.js
+++ server/api/config/index.js
@@ -1,3 +1,23 @@
-module.exports=async(app)=>{
-    require('../src/routes')(app)
-}
+const { Console } = require("console");
+const mongoose = require("mongoose");
+
+module.exports = async (app) => {
+  require("../src/routes")(app);
+
+  mongoose.connect(
+      'mongodb://mongodb:27017/MERN-APP',
+      {
+          useNewUrlParser:true,
+          useUnifiedTopology:true,
+      },
+      (err)=>{
+          if(err){
+              console.error('FAILED TO CONNECT MONGODB')
+              console.error(err)
+          }else{
+              console.log('CONNECTED TO MONGODB')
+              app.listen(8080,()=>{console.log("server is running on port 8080")})
+          }
+      }
+  );
+};
--- server/api/src/index.js
+++ server/api/src/index.js
@@ -4,5 +4,5 @@ const app=express()
 
 require('../config')(app)
 
-app.listen(80,()=>{console.log("server is running on port 3000")})
+
 module.exports=app
