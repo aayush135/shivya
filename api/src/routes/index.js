371adeee136b4f2bba2ff8aa61b37cf9578f8de2
asnk <singh.asnk@gmail.com>
2022-06-30 04:09:02 +0530

    implemented multithreading via worker threads

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ import user from './user'
 import login from './login'
 import logout from './logout'
 import signup from './signup'
+import fibonacci from './fibonacci'
 
 export default (app)=>{
     
@@ -9,6 +10,7 @@ export default (app)=>{
     app.use('/api/login',login)
     app.use('/api/logout',logout)
     app.use('/api/signup',signup)
+    app.use('/api/fibonacci',fibonacci)
     
     app.get('/*',(req,res)=>{res.sendStatus(404)})
 }
